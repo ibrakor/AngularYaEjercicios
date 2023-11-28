85a9924355e2b1302028a1f1ecc5618870eca01d
Brahim Korsan El Ghabi <brahim.kor@educa.jcyl.es>
2023-11-28 20:05:04 +0100

    Revert "a"
    This reverts commit 8b2a8f604d26ceb28c0009a9e6b4ffcf81baec8f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Punto7Component } from './components/punto-7/punto-7.component';
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Punto7Component
+    AppComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
