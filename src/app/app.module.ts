901f364cd1529c4fb95dec03fc226821bd6eaf93
Brahim Korsan El Ghabi <brahim.kor@educa.jcyl.es>
2023-11-29 18:05:35 +0100

    comunicacion1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { HijoComponent } from './hijo/hijo.component';
+import { PadreComponent } from './padre/padre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HijoComponent,
+    PadreComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/padre/padre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.css']
+})
+export class PadreComponent {
+ mensaje : string = "Este mensaje se crea en el padre y se pasa al hijo"
+}
