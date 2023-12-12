1278a4aa0d7b866a8fe8723829e10e3a64e08490
Ibra <brahim.kor@educa.jcyl.es>
2023-12-12 20:08:45 +0200

    borrado dado y añadido boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import {RouterOutlet} from "@angular/router";
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import {FormsModule} from "@angular/forms";
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
